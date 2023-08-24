d79bdaa774287cbe341910165f4dda85778214ff
tkitsolutions <mail.tkitsolutions@gmail.com>
2023-08-24 22:35:42 +0500

    Updated Responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Logo1 from "../assets/images/tkitlogo.svg";
 import Logo2 from "../assets/images/logo2.svg";
 import menuicon from "../assets/images/menu.svg";
 import {RxHamburgerMenu} from 'react-icons/rx';
-const Navbar = () => {
+const Navbar = ({homeRef,aboutRef,productsRef,servicesRef}) => {
 
   const handleClick=()=>{
     const x=document.getElementById("menu");
@@ -19,10 +19,10 @@ const Navbar = () => {
         <img src={Logo1} alt=""  className='logo1' />
         <img src={Logo2} alt=""   className='logo2' />
        <ul id="menu">
-        <li><a>Home</a></li>
-        <li><a>About Us</a></li>
-        <li><a>Services</a></li>
-        <li><a>Products</a></li>
+        <li><a onClick={()=>homeRef.scrollIntoView({behaviour:"smooth"})}>Home</a></li>
+        <li><a onClick={()=>aboutRef.scrollIntoView({behaviour:"smooth"})}>About Us</a></li>
+        <li><a onClick={()=>servicesRef.scrollIntoView({behaviour:"smooth"})}>Services</a></li>
+        <li><a onClick={()=>productsRef.scrollIntoView({behaviour:"smooth"})}>Products</a></li>
       </ul>
 
       <div className="rightSide">
